ce8a8a81b1f49b679102fa07ff3055f56387044b
wrnlb666 <wrnlb666@gmail.com>
2024-07-13 01:53:47 +0800

    lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
     -- my plugin start
+
+    -- codeium
     {
         'Exafunction/codeium.vim',
         event = 'BufEnter',
@@ -16,6 +18,27 @@ return {
                 }
             )
         end,
+    },
+
+    -- lazygit
+    {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
     },
 
     -- my plugin stop
