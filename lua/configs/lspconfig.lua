4a1dcf17ec093978b69a97d90842751e822e7b90
wrnlb666 <wrnlb666@gmail.com>
2025-02-16 23:14:47 +1100

    set html

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,93 +1,114 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local servers = {
-    "gopls",
-    "golangci_lint_ls",
-    "templ",
-    "buf_ls",
-    "pyright",
-    "ruff",
-    "omnisharp",
-    "zls",
-    "ols",
-    "dockerls",
-    "docker_compose_language_service",
-    "sqls",
-    "ts_ls",
-    "tailwindcss",
-    "html",
-    "cssls",
+	"gopls",
+	"golangci_lint_ls",
+	"templ",
+	"buf_ls",
+	"pyright",
+	"ruff",
+	"omnisharp",
+	"zls",
+	"ols",
+	"dockerls",
+	"docker_compose_language_service",
+	"sqls",
+	"ts_ls",
+	"tailwindcss",
+	"cssls",
 }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = on_attach,
-        on_init = on_init,
-        capabilities = capabilities,
-    }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		on_init = on_init,
+		capabilities = capabilities,
+	})
 end
 
 -- clangd
-lspconfig.clangd.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = {
-        "clangd",
-        "--offset-encoding=utf-16",
-        "--function-arg-placeholders=0",
-        "--clang-tidy",
-        "--malloc-trim",
-        "--completion-style=detailed"
-    },
-    filetypes = {
-        "c",
-        "cpp",
-        "objc",
-        "objcpp",
-        "cuda",
-    },
-}
+lspconfig.clangd.setup({
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
+	cmd = {
+		"clangd",
+		"--offset-encoding=utf-16",
+		"--function-arg-placeholders=0",
+		"--clang-tidy",
+		"--malloc-trim",
+		"--completion-style=detailed",
+	},
+	filetypes = {
+		"c",
+		"cpp",
+		"objc",
+		"objcpp",
+		"cuda",
+	},
+})
+
+-- html
+lspconfig.html.setup({
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
+	settings = {
+		html = {
+			format = {
+				indentInnerHtml = true,
+				templating = true,
+				wrapLineLength = 120,
+				wrapAttributes = "auto",
+				indentHandlebars = false,
+				endWithNewline = true,
+				extraLiners = "head, body, /html",
+				indent_size = 4,
+			},
+		},
+	},
+})
 
 -- rust_analyzer
 lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        ["rust-analyzer"] = {
-            diagnostics = {
-                enable = false;
-            },
-            assist = {
-                importGranularity = "module",
-                importMergeBehavior = "last",
-                importPrefix = "by_self",
-            },
-            cargo = {
-                loadOutDirsFromCheck = true,
-            },
-            procMacro = {
-                enable = true,
-            },
-            completion = {
-                autoimport = {
-                    enable = true,
-                },
-                addCallArgumentSnippets = true,
-                postfix = {
-                    enable = true,
-                },
-            },
-            -- Optional: Enable inlay hints for better context
-            inlayHints = {
-                chainingHints = true,
-                parameterHints = true,
-                typeHints = true,
-            },
-        }
-    },
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
+	settings = {
+		["rust-analyzer"] = {
+			diagnostics = {
+				enable = false,
+			},
+			assist = {
+				importGranularity = "module",
+				importMergeBehavior = "last",
+				importPrefix = "by_self",
+			},
+			cargo = {
+				loadOutDirsFromCheck = true,
+			},
+			procMacro = {
+				enable = true,
+			},
+			completion = {
+				autoimport = {
+					enable = true,
+				},
+				addCallArgumentSnippets = true,
+				postfix = {
+					enable = true,
+				},
+			},
+			-- Optional: Enable inlay hints for better context
+			inlayHints = {
+				chainingHints = true,
+				parameterHints = true,
+				typeHints = true,
+			},
+		},
+	},
 })
