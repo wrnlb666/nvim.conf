335eb2c584ce392b668e73a5c12ef670a68179db
wrnlb666 <wrnlb666@gmail.com>
2025-08-09 04:13:36 +1000

    update config for 'multi-cursor'

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,6 +56,20 @@ return {
 			mc.setup()
 			local set = vim.keymap.set
 
+			-- Add or skip cursor above/below the main cursor.
+			set({ "n", "x" }, "<up>", function()
+				mc.lineAddCursor(-1)
+			end)
+			set({ "n", "x" }, "<down>", function()
+				mc.lineAddCursor(1)
+			end)
+			set({ "n", "x" }, "<leader><up>", function()
+				mc.lineSkipCursor(-1)
+			end)
+			set({ "n", "x" }, "<leader><down>", function()
+				mc.lineSkipCursor(1)
+			end)
+
 			-- Add or skip adding a new cursor by matching word/selection
 			set({ "n", "x" }, "<leader>N", function()
 				mc.matchAddCursor(1)
