31d0bf37e036d9a7669b41c84c742c989f1829cd
wrnlb666 <wrnlb666@gmail.com>
2025-08-31 04:08:16 +1000

    update installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
 
-if [ ! -d ~/.config ]; then
-    mkdir ~/.config
-fi
-
-if [ -e ~/.config/nvim ]; then
-    rm -rf ~/.config/nvim
-fi
+[ -d ~/.config ] || mkdir ~/.config
+[ -e ~/.config/nvim ] && rm -rf ~/.config/nvim
 
 ln -sf $(pwd) ~/.config/nvim
