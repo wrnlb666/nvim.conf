dc36a0a52151dcabed2281bd7163bc7ecf8d6a51
wrnlb666 <wrnlb666@gmail.com>
2026-03-25 01:26:09 +1100

    format code, change auto quit logic

--- lua/opt-nvim-tree.lua
+++ lua/opt-nvim-tree.lua
@@ -1,55 +1,49 @@
 -- nvim tree automatic behavior
 require("nvim-tree").setup({
-    filters = { dotfiles = false },
-    disable_netrw = true,
-    hijack_cursor = true,
-    sync_root_with_cwd = true,
-    update_focused_file = {
-        enable = true,
-        update_root = false,
-    },
-    view = {
-        width = 30,
-        preserve_window_proportions = true,
-    },
-    renderer = {
-        root_folder_label = false,
-        highlight_git = true,
-        indent_markers = { enable = true },
-        icons = {
-            glyphs = {
-                default = "󰈚",
-                folder = {
-                    default = "",
-                    empty = "",
-                    empty_open = "",
-                    open = "",
-                    symlink = "",
-                },
-                git = { unmerged = "" },
-            },
-        },
-    },
-    actions = {
-        remove_file = {
-            close_window = false,
-        },
-    },
+	filters = { dotfiles = false },
+	disable_netrw = true,
+	hijack_cursor = true,
+	sync_root_with_cwd = true,
+	update_focused_file = {
+		enable = true,
+	},
+	view = {
+		width = 30,
+		preserve_window_proportions = true,
+	},
+	renderer = {
+		root_folder_label = false,
+		highlight_git = "all",
+		indent_markers = { enable = true },
+		icons = {
+			glyphs = {
+				default = "󰈚",
+				folder = {
+					default = "",
+					empty = "",
+					empty_open = "",
+					open = "",
+					symlink = "",
+				},
+				git = { unmerged = "" },
+			},
+		},
+	},
+	actions = {
+		remove_file = {
+			close_window = false,
+		},
+	},
 })
 
--- open nvim-tree automatically
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-    nested = true,
-    callback = function(data)
-        local _ = data
-        -- setup exit autocmd
-        vim.api.nvim_create_autocmd("BufEnter", {
-            nested = true,
-            callback = function()
-                if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "NvimTree" then
-                    vim.cmd("quit")
-                end
-            end,
-        })
-    end,
+-- close nvim-tree automatically
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+	pattern = "NvimTree_*",
+	callback = function()
+		local layout = vim.api.nvim_call_function("winlayout", {})
+		if layout[1] == "leaf" and vim.bo.filetype == "NvimTree" and layout[3] == nil then
+			vim.cmd("confirm quit")
+		end
+	end,
 })
