ec039b9145fc55be74befa6e986104ef91962167
wrnlb666 <wrnlb666@gmail.com>
2026-03-25 01:25:48 +1100

    add noice

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,6 +19,28 @@ return {
 		end,
 	},
 
+	-- noice
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+	},
+
 	-- mdmath
 	{
 		"Thiago4532/mdmath.nvim",
